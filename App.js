86218a00ec7dfe7fe17151513c6cb876a0ebcf42
Nelson Maldonado <play2nino@gmail.com>
2023-12-11 21:26:39 -0800

    this app is only to test sounds buttons etc..

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { Button, StyleSheet, Text, View } from "react-native"
+import { Audio } from "expo-av"
+import React, { useEffect, useState } from "react"
 
 export default function App() {
+  const [sound, setSound] = React.useState(null)
+
+  useEffect(() => {
+    const fetchSound = async () => {
+      const { sound } = await Audio.Sound.createAsync(
+        require("./assets/sounds/sound.mp3")
+      )
+      setSound(sound)
+    }
+
+    fetchSound()
+  }, [])
+
+  const startTime = 15
+  let currentDate = new Date()
+  const currentTime = currentDate.getHours()
+
+  const playSound = async () => {
+    if (sound) {
+      await sound.stopAsync()
+      await sound.playAsync()
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>App by Nelly</Text>
+      <StatusBar style="light-content" />
+
+      <View>
+        <Button onPress={playSound} title="Click Me" />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
