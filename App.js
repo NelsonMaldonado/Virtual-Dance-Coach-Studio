3acf8d4710e766125fd486273a6e9cef38f70abe
Nelson Maldonado <play2nino@gmail.com>
2023-12-15 19:24:16 -0800

    app still needs dropdown memu"

--- App.js
+++ App.js
@@ -11,7 +11,6 @@ import {
 import { Audio } from "expo-av"
 import React, { useState } from "react"
 import { Image } from "expo-image"
-import PickSong from "./assets/components/pickSong"
 
 import {
   sonidoBestialArray,
@@ -113,7 +112,7 @@ export default function App() {
       />
       <Text style={styles.title}>SalsaColombia Dance Academy</Text>
       {/* Modal goes here */}
-      <PickSong />
+
       {/* Modal ends here */}
       <Text style={styles.subTitle}> {titu}</Text>
 
